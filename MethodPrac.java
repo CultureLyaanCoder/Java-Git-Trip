bccd0362a1880790868c2fa556db3498dde68d16
Takudzwa Masunda <takudzwakevv@gmail.com>
2024-11-05 15:52:10 +0200

    Trying to commit to github

--- MethodPrac.java
+++ MethodPrac.java
@@ -6,9 +6,11 @@ public class MethodPrac {
 	public static void main(String[] args) {
 		System.out.println(add(2,2));
 
+		add(4,4);
 	}
 	public static double add(double x, double y) {
 		return x+y;
 	}
+	
 	
 }
