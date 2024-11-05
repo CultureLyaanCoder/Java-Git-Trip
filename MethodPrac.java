5c6156a123f98f2fa372f531995d2cfb4053a9c7
Takudzwa Masunda <takudzwakevv@gmail.com>
2024-11-05 15:34:29 +0200

    Now added the method prac class

---
+++ MethodPrac.java
@@ -0,0 +1,14 @@
+package practise;
+
+public class MethodPrac {
+
+	
+	public static void main(String[] args) {
+		System.out.println(add(2,2));
+
+	}
+	public static double add(double x, double y) {
+		return x+y;
+	}
+	
+}
